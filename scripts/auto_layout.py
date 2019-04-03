eaac88bafb3e8a3fcfe2674545965e3a46ffa107
Denis Zheleztsov <d_zheleztsov@wargaming.net>
2019-04-03 11:55:33 +0300

    i3blocks script

---
+++ scripts/auto_layout.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# This is a simple i3blocks script
+
+import json
+import os
+
+
+LAYOUTS = {
+    "manual": "[M]",
+    "spiral": "[S]",
+    "failed": "[DOWN]"
+}
+
+
+def auto_layout_cmd(cmd):
+    """sends a command to the swaymgr"""
+    return os.popen("swaymgr -s '" + cmd + "'").read()
+
+
+def get_current_layout():
+    """get the currently active workspace layout configuration"""
+    try:
+        layout = json.loads(auto_layout_cmd("get layout"))
+    except Exception:
+        print(LAYOUTS["failed"])
+        return
+    if not layout["managed"]:
+        print(LAYOUTS["manual"])
+    else:
+        print(LAYOUTS[layout["layout"]])
+
+
+if __name__ == "__main__":
+    get_current_layout()
